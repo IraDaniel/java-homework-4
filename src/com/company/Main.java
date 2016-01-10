1158153ad5eec74424d6072c7a94a932f5b7f8db
IrinaDaniel <ira-daniel@yandex.ru>
2016-01-10 15:43:16 +0300

    gg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,22 @@
 package com.company;
 
-import com.company.Task1.FileInputOutputStream;
-import com.company.Task2.ReaderWriter;
+import com.company.task1.FileInputOutputStream;
+import com.company.task2.ReaderWriter;
+import com.company.task3.ReaderWriterEncode;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-       /* FileInputOutputStream fileInputOutputStream = new FileInputOutputStream("Notebook.java","result1.txt");
-        fileInputOutputStream.read();
-        fileInputOutputStream.write();
+        FileInputOutputStream fileInputOutputStream = new FileInputOutputStream();
+        fileInputOutputStream.run("Notebook.java","result1.txt");
 
-        ReaderWriter readerWriter = new ReaderWriter("Notebook.java","result2.txt");
-        readerWriter.read();
-        readerWriter.write();
-        */
+        ReaderWriter readerWriter = new ReaderWriter();
+        readerWriter.run("Notebook.java","result2.txt");
 
-        Map<String,Integer> mp = new HashMap<>();
-        mp.put("a",1);
-        mp.put("b",1);
-
-        System.out.println(mp);
-        mp.put("a",2);
-        System.out.println(mp);
-
-        /*
-        * BufferedReader bufReader = new BufferedReader(new InputStreamReader(new FileInputStream(tmpFile), Charset.forName("UTF-8")));
-        * */
-
-	// write your code here
+        ReaderWriterEncode readerWriterEncode = new ReaderWriterEncode();
+        readerWriterEncode.run("new.txt", "a.txt");
     }
 }
